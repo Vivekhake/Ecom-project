a79d435ba4213adc3ed3fe5a6200ea8e0ebcd3f5
Vivekhake <162538476+Vivekhake@users.noreply.github.com>
2025-07-10 10:57:49 +0530

    navbar_changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastMessageComponent } from './toast-message/toast-message.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastMessageComponent, NavbarComponent],
+  imports: [RouterOutlet, ToastMessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: '', redirectTo: '/navbar', pathMatch: 'full' }, // ✅ Fixed
+];
