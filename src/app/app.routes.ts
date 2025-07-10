5f3862f4aac4ddad6e50024913b0ede4f76a8e36
Vivekhake <162538476+Vivekhake@users.noreply.github.com>
2025-07-10 13:23:34 +0530

    home_page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,4 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'navbar', component: NavbarComponent },
-  { path: '', redirectTo: '/navbar', pathMatch: 'full' }, // ✅ Fixed
-];
+export const routes: Routes = [];
